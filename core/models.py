df2755c143118cb66112fbfa665cf3d05f730d48
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-26 15:39:55 -0500

    Add created_at and updxated_at to model.

--- core/models.py
+++ core/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=140)
     body =  models.TextField(max_length=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Note title: {self.title} body: {self.body}"
