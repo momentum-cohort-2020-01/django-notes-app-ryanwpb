0aff0f7996d7856bce1565f6a40630bb6262a828
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-27 10:33:07 -0500

    Style main div.

--- core/static/main.js
+++ core/static/main.js
@@ -1,6 +1,7 @@
 let lines = document.querySelector("#lines");
-menu = document.querySelector(".mobile-menu");
-main = document.querySelector("main");
+let menu = document.querySelector(".mobile-menu");
+let main = document.querySelector("main");
+
 lines.addEventListener("click", function() {
   menu.style.left = "0";
   menu.classList.add("toggled");
