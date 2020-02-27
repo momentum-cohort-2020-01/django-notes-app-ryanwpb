f70dfe33cfd265942ed8f103f0037490db208f25
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-27 10:05:39 -0500

    Move mobile menu left.

--- core/static/main.js
+++ core/static/main.js
@@ -2,11 +2,11 @@ let lines = document.querySelector("#lines");
 menu = document.querySelector(".mobile-menu");
 main = document.querySelector("main");
 lines.addEventListener("click", function() {
-  menu.style.right = "0";
+  menu.style.left = "0";
   menu.classList.add("toggled");
 });
 
 main.addEventListener("click", function() {
-  menu.style.right = "-270px";
+  menu.style.left = "-270px";
   menu.classList.remove("toggled");
 });
